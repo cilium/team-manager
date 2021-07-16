cilium/team-manager

commit 874fe77169aae0105dc5b42ab7620e1b3731c1b2

check if members in teams belong to the organization
If a user is added to a team that does not belong to the organization
that user will receive an invite to belong to the organization which
is not safe.

Signed-off-by: André Martins <andre@cilium.io>

pkg/config/config.go
@@ -14,6 +14,10 @@
 
 package config
 
+import (
+	"fmt"
+)
+
 type Config struct {
 	// Organization being managed.
 	Organization string `json:"organization,omitempty" yaml:"organization,omitempty"`
@@ -82,3 +86,22 @@ const (
 	TeamReviewAssignmentAlgorithmLoadBalance TeamReviewAssignmentAlgorithm = "LOAD_BALANCE"
 	TeamReviewAssignmentAlgorithmRoundRobin  TeamReviewAssignmentAlgorithm = "ROUND_ROBIN"
 )
+
+// SanityCheck checks if the all team members belong to the organization.
+func SanityCheck(cfg *Config) error {
+	// Check if all users in the CodeReviewAssignment belong to the list of
+	// members
+	for teamName, team := range cfg.Teams {
+		for _, member := range team.Members {
+			if _, ok := cfg.Members[member]; !ok {
+				return fmt.Errorf("member %q from team %q does not belong to organization", member, teamName)
+			}
+		}
+		for _, xMember := range team.CodeReviewAssignment.ExcludedMembers {
+			if _, ok := cfg.Members[xMember.Login]; !ok {
+				return fmt.Errorf("member %q from code review assignment of team %q does not belong to organization", xMember.Login, teamName)
+			}
+		}
+	}
+	return nil
+}

cmd/main.go
@@ -63,6 +63,7 @@ func main() {
 
 	var newConfig *config.Config
 	localCfg, err := persistence.LoadState(configFilename)
+
 	switch {
 	case errors.Is(err, os.ErrNotExist):
 		fmt.Printf("Configuration file %q not found, retriving configuration from organization...\n", configFilename)
@@ -74,8 +75,16 @@ func main() {
 	case err != nil:
 		panic(err)
 	case dryRun:
+		err = config.SanityCheck(localCfg)
+		if err != nil {
+			panic(err)
+		}
 		newConfig = localCfg
 	default:
+		err = config.SanityCheck(localCfg)
+		if err != nil {
+			panic(err)
+		}
 		newConfig, err = tm.SyncTeams(globalCtx, localCfg, force)
 		if err != nil {
 			panic(err)

commit 99fb6116ce17c77fc88c486bfe56c744b7e1ff66

pkg/config: check if globally excluded members belong to the org
Fixes: 874fe77169aa ("check if members in teams belong to the organization")
Signed-off-by: André Martins <andre@cilium.io>

pkg/config/config.go
@@ -103,5 +103,10 @@ func SanityCheck(cfg *Config) error {
 			}
 		}
 	}
+	for _, xMember := range cfg.ExcludeCRAFromAllTeams {
+		if _, ok := cfg.Members[xMember]; !ok {
+			return fmt.Errorf("member %q from globally excluded reviews, does not belong to the organization", xMember)
+		}
+	}
 	return nil
 }