53585a10a985db8949092b5dfc1307a5f0e8aa2b
Marco Hofstetter <marco.hofstetter@isovalent.com>
2023-06-27 13:59:39 +0200

    cmd: remove main command
    This commit removes legacy team-manager functionality from the root
    command. There should be dedicated cobra commands for all actions.
    
    Signed-off-by: Marco Hofstetter <marco.hofstetter@isovalent.com>

--- cmd/main.go
+++ cmd/main.go
@@ -16,17 +16,10 @@ package main
 
 import (
 	"context"
-	"errors"
-	"fmt"
 	"os"
 	"os/signal"
 
 	"github.com/spf13/cobra"
-
-	"github.com/cilium/team-manager/pkg/config"
-	"github.com/cilium/team-manager/pkg/github"
-	"github.com/cilium/team-manager/pkg/persistence"
-	"github.com/cilium/team-manager/pkg/team"
 )
 
 var (
@@ -34,16 +27,11 @@ var (
 	configFilename string
 	force          bool
 	dryRun         bool
-	addUsers       []string
 	addTeams       []string
-	setTopHat      []string
-	addPTO         []string
-	removePTO      []string
 
 	rootCmd = &cobra.Command{
 		Use:   "team-manager",
 		Short: "Manage GitHub team state locally and synchronize it with GitHub",
-		RunE:  run,
 	}
 )
 
@@ -54,81 +42,7 @@ func init() {
 	flag.StringVar(&configFilename, "config-filename", "team-assignments.yaml", "Config filename")
 	flag.BoolVar(&force, "force", false, "Force local changes into GitHub without asking for configuration")
 	flag.BoolVar(&dryRun, "dry-run", false, "Dry run the steps without performing any write operation to GitHub")
-	flag.StringSliceVar(&addUsers, "add-users", nil, "Adds new users to the configuration file")
 	flag.StringSliceVar(&addTeams, "add-teams", nil, "Adds new teams to the configuration file")
-	flag.StringSliceVar(&setTopHat, "set-top-hat", nil, "Sets the the members of the top hat team")
-	flag.StringSliceVar(&addPTO, "add-pto", nil, "Add users on PTO")
-	flag.StringSliceVar(&removePTO, "remove-pto", nil, "Remove users from PTO")
-}
-
-func run(cmd *cobra.Command, args []string) error {
-	ghClient, err := github.NewClientFromEnv()
-	if err != nil && !dryRun {
-		return fmt.Errorf("failed to create github client: %w", err)
-	}
-
-	ghGraphQLClient, err := github.NewClientGraphQLFromEnv()
-	if err != nil && !dryRun {
-		return fmt.Errorf("failed to create github graphql client: %w", err)
-	}
-
-	tm := team.NewManager(ghClient, ghGraphQLClient, orgName)
-
-	localCfg, err := persistence.LoadState(configFilename)
-	var newConfig = localCfg
-	if err != nil {
-		if !errors.Is(err, os.ErrNotExist) {
-			return fmt.Errorf("failed to load local state: %w", err)
-		}
-
-		fmt.Printf("Configuration file %q not found, retriving configuration from organization...\n", configFilename)
-		newConfig, err = tm.GetCurrentConfig(cmd.Context())
-		if err != nil {
-			return fmt.Errorf("failed to read config from GitHub: %w", err)
-		}
-		fmt.Printf("Done, change your local configuration and re-run me again.\n")
-	}
-
-	switch {
-	case dryRun || len(addUsers) != 0 || len(addTeams) != 0 ||
-		len(setTopHat) != 0 || len(addPTO) != 0 || len(removePTO) != 0:
-		newConfig = localCfg
-
-		if err = addUsersToConfig(cmd.Context(), addUsers, newConfig, ghClient); err != nil {
-			return fmt.Errorf("failed to add users: %w", err)
-		}
-
-		if err = addTeamsToConfig(cmd.Context(), addUsers, newConfig, ghClient); err != nil {
-			return fmt.Errorf("failed to add teams: %w", err)
-		}
-
-		if len(setTopHat) > 0 {
-			if err = setTeamMembers("tophat", setTopHat, newConfig); err != nil {
-				return fmt.Errorf("failed to set tophat team members: %w", err)
-			}
-		}
-
-		if err = addCRAExclusionToConfig(addPTO, newConfig); err != nil {
-			return fmt.Errorf("failed to add code review assignment exclusion: %w", err)
-		}
-		if err = removeCRAExclusionToConfig(removePTO, newConfig); err != nil {
-			return fmt.Errorf("failed to remove code review assignment exclusion: %w", err)
-		}
-	default:
-		err = config.SanityCheck(localCfg)
-		if err != nil {
-			return fmt.Errorf("failed to perform sanity check: %w", err)
-		}
-		newConfig, err = tm.SyncTeams(cmd.Context(), localCfg, force)
-		if err != nil {
-			return fmt.Errorf("failed to sync teams to GitHub: %w", err)
-		}
-	}
-	if err = persistence.StoreState(configFilename, newConfig); err != nil {
-		return fmt.Errorf("failed to store state to config: %w", err)
-	}
-
-	return nil
 }
 
 func main() {
